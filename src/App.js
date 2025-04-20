a81ee67a3d530885ecc9a909d40ebd8684c63698
Justin Tran <justin.s.tran97@gmail.com>
2025-04-20 13:39:13 -0700

    Added custom pattern for particlesbg

--- src/App.js
+++ src/App.js
@@ -122,9 +122,42 @@ class App extends React.Component {
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
 
+    let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      alpha: [0.6, 0],
+      scale: [.1, 0.4],
+      position: "all",
+      color: ["random", "#FFFEFE"],
+      cross: "dead",
+      // emitter: "follow",
+      random: 1
+    };
+
+    if (Math.random() > 0.85) {
+      config = Object.assign(config, {
+        onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(
+            particle.p.x,
+            particle.p.y,
+            particle.radius * 2,
+            particle.radius * 2
+          );
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+        }
+      });
+    }
+
     return (
       <div className='App'>
-        <ParticlesBg color='##FFFEFE' type='cobweb' bg={true} />
+        <ParticlesBg type="custom" config={config} bg={true} />
         <Navigation
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}/>
